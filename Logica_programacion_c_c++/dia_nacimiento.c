577eee85c32d834ecae6fb55ce6eed2ba4885e24
jhonholberton <3425@holbertonschool.com>
2022-03-28 18:03:56 -0500

    basico/solicitar dia de nacimeinto del ususario

---
+++ Logica_programacion_c_c++/dia_nacimiento.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* solicitud dia de nacimiento de el usuario*/
+
+int main()
+{
+int dia_nacimiento;
+printf("Introduce tu dia de nacimiento; \n");
+scanf("%i", &dia_nacimiento);
+printf("El dia de nacimiento es: %i\n", dia_nacimiento);
+getchar();
+}
