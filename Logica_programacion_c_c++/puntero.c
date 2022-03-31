58d46c7c4d19eda80affbb560928887000b523fd
jhonholberton <3425@holbertonschool.com>
2022-03-31 17:40:24 -0500

    solicitud de direccion de memoria mendiante punteros

--- Logica_programacion_c_c++/puntero.c
+++ Logica_programacion_c_c++/puntero.c
@@ -8,7 +8,7 @@ int main()
 	p = &numero;
 	printf("introduce un numero\n");
 	scanf("%i", &numero);
-	printf("Direccion en memoria de la variable numero es: %x\n", p);
-	printf("el numero contenido en la variable es: %i\n", numero);
+	printf("Direccion en memoria de la variable numero es: %p\n", p);
+	printf("el numero contenido en la variable es: %i\n", *p);
 	getchar();
 }
